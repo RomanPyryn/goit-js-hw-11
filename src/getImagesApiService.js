d1a983aa4ea433e3a2b6856f2d750343057bb8c3
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2022-06-04 15:12:52 +0300

    Adds final corrections

--- src/getImagesApiService.js
+++ src/getImagesApiService.js
@@ -1,25 +1,23 @@
 import axios from 'axios';
 
-// export function getImages(request) {
-//     return axios(`https://pixabay.com/api/?key=27640726-9b55f8bbb95505cb3dfdadc58&q=${request}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`); 
-// };
-
 export default class getImagesApiService {
     constructor() {
         this.request = '';
         this.page = 1
     }
 
-    getImages() {
+    getData({ data }) {
+        this.incrementPage();
+        return data;
+    };
+
+    async getImages() {
         const url = `https://pixabay.com/api/?key=27640726-9b55f8bbb95505cb3dfdadc58&q=${this.request}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+        const response = await axios(url);
+        const data = await this.getData(response);
+        return data;
         
-        return axios(url)
-            .then(({data}) => {
-                this.incrementPage();
-                return data;
-            });
-        
-       
+        // axios(url).then(({ data }) => {this.incrementPage();return data;});
          
     }
 
